5720f2edbcabf4273d83a9dc2cb3c7be59abe442
Jon <github@alleskaputt.com>
2016-12-25 11:10:48 +0100

    ported vim to vundle

--- install/backup.sh
+++ install/backup.sh
@@ -17,7 +17,7 @@ for file in $linkables; do
     target="$HOME/$filename"
     if [ ! -L $target ]; then
         echo "backing up $filename"
-        cp $target $BACKUP_DIR
+        cp -rf $target $BACKUP_DIR
     else
         echo -e "$filename does not exist at this location or is a symlink"
     fi
