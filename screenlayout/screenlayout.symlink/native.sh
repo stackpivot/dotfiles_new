e8b60862d768dff5875b35ab1b9f9d2c3be6fede
Jon <git@alleskaputt.com>
2018-04-23 22:21:24 +0400

    added screenlayout for xrandr

---
+++ screenlayout/screenlayout.symlink/native.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1 --off --output DVI-I-1-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1400x1050 --pos 0x0 --rotate normal
---
+++ screenlayout/screenlayout.symlink/portable_landscape.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1 --off --output DVI-I-1-1 --mode 1920x1080 --pos 1400x168 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 1400x1050 --pos 0x0 --rotate normal
